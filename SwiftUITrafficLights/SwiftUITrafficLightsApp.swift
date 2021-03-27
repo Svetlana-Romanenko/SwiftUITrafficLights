662bd00668c4e27cfbc99b51c77599a03dc44c26
Svetlana <svetaromanenko@inbox.ru>
2021-03-27 19:42:09 +0300

    Initial Commit

---
+++ SwiftUITrafficLights/SwiftUITrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrafficLightsApp.swift
+//  SwiftUITrafficLights
+//
+//  Created by Светлана Романенко on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
